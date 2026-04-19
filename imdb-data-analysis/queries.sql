017bac5831c98f4b7cab62bd37293688a3667534
Cristian Jimenez <crisjimenez445@gmail.com>
2026-04-18 23:40:00 -0400

    Update queries.sql

--- imdb-data-analysis/queries.sql
+++ imdb-data-analysis/queries.sql
@@ -6,6 +6,7 @@ WHERE numvotes > 50000
 ORDER BY averagerating DESC
 LIMIT 50;
 
+
 SELECT genres, COUNT(*) AS title_count, AVG(averagerating), AVG(numvotes) AS avg_votes
 FROM title_basics AS b
 JOIN title_ratings AS r
@@ -16,12 +17,14 @@ HAVING COUNT(primarytitle) >= 50
 ORDER BY AVG(averagerating) DESC
 LIMIT 15;
 
+
 SELECT titletype, COUNT(*) AS title_count, AVG(averagerating) AS avg_rating, AVG(numvotes) AS avg_votes
 FROM title_basics AS b
 JOIN title_ratings AS r
     ON b.tconst = r.tconst
 WHERE numvotes >= 10000 AND titletype IN ('movie', 'tvSeries', 'tvMiniSeries')
 GROUP BY titletype;
+
 
 SELECT startyear, COUNT(*) AS title_count, AVG(averagerating) AS avg_rating
 FROM title_basics AS b
@@ -30,6 +33,7 @@ JOIN title_ratings AS r
 WHERE startyear IS NOT NULL AND numvotes >= 10000 AND startyear BETWEEN 1980 AND 2025
 GROUP BY startyear
 HAVING COUNT(*) >= 100;
+
 
 SELECT COUNT(*) AS title_count, AVG(averagerating) AS avg_rating,
        CASE
@@ -45,6 +49,7 @@ JOIN title_ratings AS r
 WHERE titletype = 'movie' AND runtimeminutes IS NOT NULL AND numvotes >= 10000
 GROUP BY runtime_bucket;
 
+
 SELECT COUNT(p.tconst) AS appearances, primaryname
 FROM title_principals AS p
 JOIN title_ratings AS r
@@ -55,3 +60,19 @@ WHERE category IN ('actor', 'actress') AND r.averagerating >= 8.5 AND r.numvotes
 GROUP BY primaryname
 ORDER BY COUNT(p.tconst) DESC
 LIMIT 20;
+
+
+SELECT n.primaryname,
+       COUNT(*) AS appearances,
+       ROUND(AVG(r.averagerating), 2) AS avg_rating
+FROM title_principals p
+JOIN title_ratings r ON p.tconst = r.tconst
+JOIN title_basics b ON p.tconst = b.tconst
+JOIN name_basics n ON p.nconst = n.nconst
+WHERE p.category IN ('actor', 'actress')
+  AND r.numvotes >= 100000
+  AND b.titletype = 'movie'
+GROUP BY n.primaryname
+HAVING COUNT(*) >= 5
+ORDER BY appearances DESC
+LIMIT 15;
